a8bcea6d459ffce44fb38553c3e61203d9c6b591
Mark <learncode23@gmail.com>
2020-08-30 14:29:33 +0100

    Started on level system
    Render issue outstanding

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,3 +1,13 @@
 export function loadImage(url) {
-	// move load image here
+	return new Promise((resolve) => {
+		const image = new Image();
+		image.addEventListener('load', () => {
+			resolve(image);
+		});
+		image.src = url;
+	});
+}
+
+export function loadLevel(name) {
+	return fetch(`/levels/${name}.json`).then((r) => r.json());
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,27 @@
-function loadImage(url) {
-	return new Promise((resolve) => {
-		const image = new Image();
-		image.addEventListener('load', () => {
-			resolve(image);
-		});
-		image.src = url;
-	});
-}
+import SpriteSheet from './SpriteSheet.js';
+import { loadImage, loadLevel } from './loaders.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-context.fillRect(0, 0, 50, 50);
+function drawBackground(background, context, sprites) {
+	background.ranges.forEach(([x1, x2, y1, y2]) => {
+		for (let x = x1; x < x2; ++x) {
+			for (let y = y1; y < y2; ++y) {
+				sprites.drawTile(background.tile, context, x, y);
+			}
+		}
+	});
+}
 
-loadImage('/image/tiles.png').then((image) => {
-	context.drawImage(image, 0, 0, 16, 16, 0, 0, 16, 16);
+loadImage('/img/tiles.png').then((image) => {
+	const sprites = new SpriteSheet(image);
+	sprites.define('ground', 0, 0);
+	sprites.define('sky', 3, 23);
+
+	loadLevel('1-1').then((level) => {
+		level.backgrounds.forEach((bg) => {
+			drawBackground(bg, context, sprites);
+		});
+	});
 });
